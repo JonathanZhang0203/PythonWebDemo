1bf5ccef40b38491be27ebc29dfdebb530dcb16d
JonathanZhang0203 <zhangcheng0203@126.com>
2020-04-02 11:26:21 +0800

    Day 2 Change

---
+++ webapp/www/app3.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  1 15:26:39 2020
+
+@author: Administrator
+"""
+
+
+import logging; logging.basicConfig(level=logging.INFO)
+
+import asyncio, os, json, time
+from datetime import datetime
+
+from aiohttp import web
+
+def index(request):
+    return web.Response(body=b'<h1>web app2</h1>', headers={'content_type':'text/html'})
+
+async def init(loop):
+    app = web.Application(loop=loop)
+    app.router.add_route('GET', '/', index)
+    srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9000)
+    logging.info('server started at http://127.0.0.1:9000...')
+    return srv
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(init(loop))
+loop.run_forever()
